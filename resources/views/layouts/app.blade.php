0d19f145f2662dea61fc9b2a2dc8a0211e2d6494
DevAhmedHamzawy <ahmedhamzawy3@gmail.com>
2025-08-21 11:58:35 +0300

    converting admin routes to admin.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,13 +43,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('admin.login') }}">Login</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
